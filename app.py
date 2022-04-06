3f031e4a5340bba2f7122d546bcacb584f2bb65a
bintang31 <andhika.restama99@gmail.com>
2022-04-06 22:32:34 +0700

    first 02

--- app.py
+++ app.py
@@ -1,8 +1,41 @@
 # save this as app.py
-from flask import Flask
+import logging
+import psutil
+import os
+from flask import Flask, Response, request,jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:Ikancucut182!!@34.101.234.107:5432/account-service"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+process = psutil.Process(os.getpid())
+
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return jsonify({"message": "success"})
+
+@app.route("/ping")
+def ping() -> Response:
+        response = Response(response='pong', status=200, mimetype='text/plain')
+        response.headers['Cache-Control'] = 'no-store, must-revalidate'
+        if request.environ.get('SERVER_PROTOCOL') == 'HTTP/1.0':
+            response.headers['Expires'] = 0
+        return jsonify({"message": response.status})
+
+@app.route('/memory')
+def print_memory():
+    return jsonify({"memory": process.memory_info().rss})
+
+@app.route('/dbcheck')
+def testdb():
+    try:
+        db.session.query("1").from_statement("SELECT 1").all()
+        return jsonify({"message": "DB Connected"})
+    except:
+        return jsonify({"message": "DB Not Connected"})
+
+if __name__ == "__main__":
+    app.run()
